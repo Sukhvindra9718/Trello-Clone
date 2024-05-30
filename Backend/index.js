b98463d42de566b0478f0dbb01642009113ac748
vishal <dasvishal60@gmail.com>
2024-05-30 15:42:45 +0530

    Added controllers into seprate file and made few changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const authRoute = require('./routes/authRoutes');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/authRoutes");
 // const boardRoute = require('./routes/board');
 // const listRoute = require('./routes/list');
 // const cardRoute = require('./routes/card');
 // const { verifyToken } = require('./middleware/verifyToken');
 
-dotenv.config({ path: './Backend/.env.development.local' });
-
+dotenv.config({ path: "./Backend/.env.development.local" });
 
 // Connect to DB
 
-mongoose.connect(process.env.DB_CONNECT, {})
-  .then(() => console.log('Connected to DB'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect(process.env.DB_CONNECT, {})
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-// Middleware
+// Middleware\
+
+// Middleware to parse JSON bodies
 app.use(express.json());
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // Route Middleware
-app.use('/api/user', authRoute);
+app.use("/api/user", authRoute);
 // app.use('/api/board', verifyToken, boardRoute);
 // app.use('/api/list', verifyToken, listRoute);
 // app.use('/api/card', verifyToken, cardRoute);
@@ -31,44 +35,45 @@ app.use('/api/user', authRoute);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 // Event listener for uncaught exceptions
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
   process.exit(1); // exit with failure
   // Optionally, you can perform additional cleanup or logging here
 });
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+app.listen(5000, () => console.log("Server is running on port 5000"));
 
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled Promise Rejection:', err);
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Promise Rejection:", err);
   // Optionally, you can perform additional cleanup or logging here
-  // handlePromise rejection(err); 
+  // handlePromise rejection(err);
 
   // If you don't handle the exception, the application will crash then give code
   // process.exit(1);
-
 });
 
-if(process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  app.use(express.static(path.resolve(__dirname, '../Frontend-Trello/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../Frontend-Trello/dist', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "../Frontend-Trello/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../Frontend-Trello/dist", "index.html")
+    );
   });
-}else{
-  app.get('/', (req, res) => {
-    res.send('API is running...');
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
   });
 }
 // Graceful shutdown
-process.on('SIGINT', () => {
-  console.log('Server is shutting down...');
+process.on("SIGINT", () => {
+  console.log("Server is shutting down...");
   server.close(() => {
-    console.log('Server has shut down.');
+    console.log("Server has shut down.");
     process.exit(0);
   });
 });
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,69 +1,13 @@
-const router = require('express').Router();
-const User = require('../models/authModel');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const { registerValidation, loginValidation } = require('../utils/validation');
+const router = require("express").Router();
+const {
+  LoginController,
+  SignUpController,
+  LogOutController,
+} = require("../controllers/User.controller");
 
-router.post('/register', async (req, res) => {
-    try {
-        // Validate the data before we make a user
-        const { error } = registerValidation(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
+router.post("/register", SignUpController);
 
-        // Check if the user is already in the database
-        const emailExist = await User.findOne({ email: req.body.email });
-        if (emailExist) return res.status(400).send('Email already exists');
+router.post("/login", LoginController);
 
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-        // Create a new user
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        try {
-            await user.save();
-            res.send({ user: user._id });
-        } catch (err) {
-            res.status(400).send(err);
-        }
-    } catch (err) {
-        res.status(400).send({ message: err.message });
-    }
-});
-
-router.post('/login', async (req, res) => {
-    // Validate the data before we make a user
-    try {
-
-        const { error } = loginValidation(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
-
-        // Check if the email exists
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) return res.status(400).send('Email is not found');
-
-        // Password is correct
-        const validPass = await bcrypt.compare(req.body.password, user.password);
-        if (!validPass) return res.status(400).send('Invalid password');
-
-        // Create and assign a token
-        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-        res.header('auth-token', token).send(token);
-    }
-    catch (err) {
-        res.status(400).send({ message: err.message });
-    }
-})
-
-router.post('/logout', async (req, res) => {
-    try {
-        res.header('auth-token', '').send({ success: true });
-    } catch (err) {
-        res.status(400).send({ message: err.message,success:false });
-    }
-})
+router.post("/logout", LogOutController);
 module.exports = router;
