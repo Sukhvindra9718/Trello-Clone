15b57ec0d4f48c74fd83afe8e777bc4739b2f0e9
SSR14FEB <instaheri6@gmail.com>
2024-06-03 15:46:50 +0530

    login logo change

--- Frontend-Trello/src/pages/Login.jsx
+++ Frontend-Trello/src/pages/Login.jsx
@@ -66,13 +66,10 @@ function Login() {
       <section className="section_Login">
         <div className="item_holders">
           <div className="logo">
-            <img
-              src="https://media.discordapp.net/attachments/1238139567845015644/1239585010336661595/Trello_logo.svg.png?ex=66441dc0&is=6642cc40&hm=13028fe06b250a510173d7fc7d16300c06573f871f1a90f5f608bb995b6a2b7d&=&format=webp&quality=lossless&width=1440&height=411"
-              alt=""
-            />
+          <img src="src/images/Trello_logo.svg.webp" alt="" />
           </div>
           <div className="input">
-            <h1 className="text-center w-full text-2xl font-semibold">Login to continue</h1>
+            <h1 className="text-center w-full text-m font-bold">Login to continue</h1>
             <input
               type="email"
               className=" border-grey border-2 pl-4"
